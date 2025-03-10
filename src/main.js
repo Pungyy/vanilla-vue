a1175d9fe8b48359c3ab7413114bc6b8dbded507
Ibrahim Anil <ibrahimanilcontactpro@gmail.com>
2025-03-10 16:52:27 +0100

    correctif

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css' // Import du CSS
-import 'bootstrap' // Import des scripts (si nécessaire)
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 createApp(App).mount('#app')
